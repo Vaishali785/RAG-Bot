c6a4e3d97b23513472819f1d52dce5d6f8761644
Vaishali <dev.vaishali785@gmail.com>
2026-04-30 17:42:18 +0530

    added different orbs for different pages

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,35 +1,39 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import Header from "../components/layout/Header"
 
 const Layout = () => {
+	const { pathname } = useLocation()
 	return (
 		<div className="relative min-h-screen overflow-hidden">
 			{/* Floating gradient orbs */}
-			{/* <div className="pointer-events-none absolute inset-0 overflow-hidden">
-				<div
-					className="absolute -top-40 -left-40 w-[500px] h-[500px] rounded-full opacity-30 blur-3xl animate-float-slow"
-					style={{ background: "var(--gradient-orb-1)" }}
-				/>
-				<div
-					className="absolute top-[16%] -right-32 w-[450px] h-[450px] rounded-full opacity-40 blur-3xl animate-float-slower"
-					style={{ background: "var(--gradient-orb-2)" }}
-				/>
-				<div
-					className="absolute -bottom-40 left-[18%] w-[500px] h-[500px] rounded-full opacity-30 blur-3xl animate-float-slow"
-					style={{ background: "var(--gradient-orb-3)" }}
-				/>
-			</div> */}
+			{pathname === "/admin" ? (
+				<div className="pointer-events-none absolute inset-0 overflow-hidden">
+					<div
+						className="absolute -top-40 -right-32 w-[500px] h-[500px] rounded-full opacity-40 blur-3xl animate-float-slow"
+						style={{ background: "var(--gradient-orb-1)" }}
+					/>
+					<div
+						className="absolute -bottom-40 -left-32 w-[450px] h-[450px] rounded-full opacity-30 blur-3xl animate-float-slower"
+						style={{ background: "var(--gradient-orb-3)" }}
+					/>
+				</div>
+			) : (
+				<div className="pointer-events-none absolute inset-0 overflow-hidden">
+					<div
+						className="absolute -top-40 -left-40 w-[500px] h-[500px] rounded-full opacity-30 blur-3xl animate-float-slow"
+						style={{ background: "var(--gradient-orb-1)" }}
+					/>
+					<div
+						className="absolute top-[16%] -right-32 w-[450px] h-[450px] rounded-full opacity-40 blur-3xl animate-float-slower"
+						style={{ background: "var(--gradient-orb-2)" }}
+					/>
+					<div
+						className="absolute -bottom-40 left-[18%] w-[500px] h-[500px] rounded-full opacity-30 blur-3xl animate-float-slow"
+						style={{ background: "var(--gradient-orb-3)" }}
+					/>
+				</div>
+			)}
 
-			<div className="pointer-events-none absolute inset-0 overflow-hidden">
-				<div
-					className="absolute -top-40 -right-32 w-[500px] h-[500px] rounded-full opacity-40 blur-3xl animate-float-slow"
-					style={{ background: "var(--gradient-orb-1)" }}
-				/>
-				<div
-					className="absolute -bottom-40 -left-32 w-[450px] h-[450px] rounded-full opacity-30 blur-3xl animate-float-slower"
-					style={{ background: "var(--gradient-orb-3)" }}
-				/>
-			</div>
 			<main className="w-full  h-screen flex flex-col">
 				<Header />
 				<Outlet />
