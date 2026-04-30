bf6625edf890d3f72e62129b3b164ed7761ef874
Vaishali <dev.vaishali785@gmail.com>
2026-04-30 17:30:00 +0530

    added toggle page button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,26 +1,43 @@
-import { Settings, Sparkles } from "lucide-react"
-import { Link } from "react-router-dom"
+import { MessageCircle, Settings, Sparkles } from "lucide-react"
+import { useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 import { ThemeToggle } from "../ThemeToggle"
 
+type Pages = "chat" | "admin"
 const Header = () => {
+	const { pathname } = useLocation()
+	const currentPage = pathname.split("/")[1] === "" ? "chat" : "admin"
+	const [activePage, setActivePage] = useState<Pages>(currentPage ?? "chat")
 	return (
 		<header className="relative z-10 px-8 py-6 w-full shadow-header">
 			<div className=" mx-auto flex justify-between items-center">
-				<div className="flex items-center gap-2 animate-fade-up">
+				<Link to="/" className="flex items-center gap-2 animate-fade-up" aria-label="Logo">
 					<div className="w-9 h-9 rounded-xl bg-gradient-button flex items-center justify-center shadow-soft">
 						<Sparkles className="w-4 h-4 text-white" />
 					</div>
 					<span className="font-semibold text-lg tracking-tight">Lumen Co.</span>
-				</div>
+				</Link>
 
 				<div className="flex items-center gap-2">
-					<Link
-						to="/admin"
-						className="w-10 h-10 rounded-full glass hover:bg-white/80 dark:hover:bg-white/10 flex items-center justify-center transition-colors"
-						aria-label="Admin"
-					>
-						<Settings className="w-4 h-4" />
-					</Link>
+					{activePage == "chat" ? (
+						<Link
+							to="/admin"
+							className="w-10 h-10 rounded-full glass hover:bg-white/80 dark:hover:bg-white/10 flex items-center justify-center transition-colors"
+							aria-label="Admin"
+							onClick={() => setActivePage("admin")}
+						>
+							<Settings className="w-4 h-4" />
+						</Link>
+					) : (
+						<Link
+							to="/"
+							className="w-10 h-10 rounded-full glass hover:bg-white/80 dark:hover:bg-white/10 flex items-center justify-center transition-colors"
+							aria-label="Admin"
+							onClick={() => setActivePage("chat")}
+						>
+							<MessageCircle className="w-4 h-4" />
+						</Link>
+					)}
 					<ThemeToggle />
 				</div>
 			</div>
