4a412a418dad1534db86b6268917867ffebbbafb
Vaishali <dev.vaishali785@gmail.com>
2026-04-30 17:25:01 +0530

    added context provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router"
 import App from "./App.tsx"
+import { ThemeContextProvider } from "./context/ThemeContextProvider.tsx"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<ThemeContextProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ThemeContextProvider>
 	</StrictMode>,
 )
