7478a1d8807638db6e87432ab24baef3d51655d9
Vaishali <dev.vaishali785@gmail.com>
2026-04-30 17:27:40 +0530

    added theme functions and fix palette function

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,8 +1,23 @@
 import type { LocalConfig, Palette } from "../types/app-types"
 
-export const applyPalette = (p: Palette) => {
-	const isDark = document.documentElement.classList.contains("dark")
-	const v = isDark ? p.dark : p.light
+export const storeLocalConfig = (config: LocalConfig) => {
+	if (config.theme) {
+		localStorage.setItem("theme", config.theme)
+	}
+	if (config.palette) {
+		localStorage.setItem("palette", config.palette)
+	}
+}
+
+export const getLocalConfig = (config: "theme" | "palette") => {
+	// if(localStorage.getItem)
+	return localStorage?.getItem(config)
+}
+
+export const applyPalette = (p: Palette, theme?: "light" | "dark") => {
+	const isDark =
+		theme ?? getLocalConfig("theme") ?? document.documentElement.classList.contains("dark")
+	const v = isDark == "dark" ? p.dark : p.light
 	const r = document.documentElement
 	r.style.setProperty("--primary", v.primary)
 	r.style.setProperty("--accent", v.accent)
@@ -14,20 +29,5 @@ export const applyPalette = (p: Palette) => {
 	r.style.setProperty("--gradient-orb-2", v.orb2)
 	r.style.setProperty("--gradient-orb-3", v.orb3)
 	r.style.setProperty("--gradient-orb-core", v.orbCore)
-}
-
-export const storeLocalConfig = (config: LocalConfig) => {
-	if (config.theme) {
-		console.log(">>Theme")
-		localStorage.setItem("theme", config.theme)
-	}
-	if (config.palette) {
-		console.log(">>Palette", config)
-		localStorage.setItem("palette", config.palette)
-	}
-}
-
-export const getLocalConfig = (config: "theme" | "palette") => {
-	// if(localStorage.getItem)
-	return localStorage?.getItem(config)
+	r.style.setProperty("--shadow-glow-color", v.primary)
 }
