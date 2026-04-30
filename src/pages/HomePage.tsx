a5b956eef4a145f9a64e8889d8971c91e5696b01
Vaishali <dev.vaishali785@gmail.com>
2026-04-30 17:19:23 +0530

    home page with custom hook

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,55 +1,25 @@
 import { useState } from "react"
 import ChatWindow from "../components/chat/ChatWindow"
 import Input from "../components/chat/Input"
+import useFetchApi from "../hooks/useFetchApi"
 
 const HomePage = () => {
 	const [typing, setTyping] = useState(false)
+	const { result, aiThinking, fetchAPI } = useFetchApi()
+
 	const [msgs, setMsgs] = useState([
-		{ id: crypto.randomUUID(), sender: "user", text: "Hi there, what your name?" },
 		{ id: crypto.randomUUID(), sender: "ai", text: "Hi I am your AI assistant." },
-		{ id: crypto.randomUUID(), sender: "user", text: "How can you help me?" },
-		{
-			id: crypto.randomUUID(),
-			sender: "ai",
-			text: "You can ask me anything about our terms and policies.",
-		},
-		{ id: crypto.randomUUID(), sender: "user", text: "Hi there, what your name?" },
-		{ id: crypto.randomUUID(), sender: "ai", text: "Hi I am your AI assistant." },
-		{ id: crypto.randomUUID(), sender: "user", text: "How can you help me?" },
-		{
-			id: crypto.randomUUID(),
-			sender: "ai",
-			text: "You can ask me anything about our terms and policies.",
-		},
-		{ id: crypto.randomUUID(), sender: "user", text: "Hi there, what your name?" },
-		{ id: crypto.randomUUID(), sender: "ai", text: "Hi I am your AI assistant." },
-		{ id: crypto.randomUUID(), sender: "user", text: "How can you help me?" },
-		{
-			id: crypto.randomUUID(),
-			sender: "ai",
-			text: "You can ask me anything about our terms and policies.",
-		},
-		{ id: crypto.randomUUID(), sender: "user", text: "Hi there, what your name?" },
-		{ id: crypto.randomUUID(), sender: "ai", text: "Hi I am your AI assistant." },
-		{ id: crypto.randomUUID(), sender: "user", text: "How can you help me?" },
-		{
-			id: crypto.randomUUID(),
-			sender: "ai",
-			text: "You can ask me anything about our terms and policies.",
-		},
-		{ id: crypto.randomUUID(), sender: "user", text: "Hi there, what your name?" },
-		{ id: crypto.randomUUID(), sender: "ai", text: "Hi I am your AI assistant." },
-		{ id: crypto.randomUUID(), sender: "user", text: "How can you help me?" },
-		{
-			id: crypto.randomUUID(),
-			sender: "ai",
-			text: "You can ask me anything about our terms and policies.",
-		},
 	])
 	return (
 		<>
-			<ChatWindow messages={msgs} typing={typing} />
-			<Input setMsgs={setMsgs} setTyping={setTyping} />
+			<ChatWindow messages={msgs} typing={typing} aiThinking={aiThinking} />
+			<Input
+				// msgs={msgs}
+				setMsgs={setMsgs}
+				setTyping={setTyping}
+				fetchAPI={fetchAPI}
+				result={result}
+			/>
 		</>
 	)
 }
