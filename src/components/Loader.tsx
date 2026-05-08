6c287044b33c979592071cff358885d71ea9294e
Vaishali <dev.vaishali785@gmail.com>
2026-05-08 15:31:44 +0530

    added loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,20 @@
+const Loader = () => {
+	return (
+		<div className="flex items-center justify-center h-full">
+			<div className="relative">
+				<div className="relative w-20 h-20">
+					<div
+						className="absolute w-full h-full rounded-full border-[3px] border-gray-100/20 border-r-primary border-b-primary animate-spin"
+						style={{ animationDuration: "3s", animationDirection: "alternate-reverse" }}
+					/>
+					<div
+						className="absolute w-full h-full rounded-full border-[3px] border-gray-100/20 border-r-primary border-b-primary animate-spin"
+						style={{ animationDuration: "3s", animationDirection: "normal" }}
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Loader
