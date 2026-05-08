f20af082f5896e0007e22cac2afd3af181c362f1
Vaishali <dev.vaishali785@gmail.com>
2026-05-08 15:20:53 +0530

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router"
 import App from "./App.tsx"
-import { ThemeContextProvider } from "./context/ThemeContextProvider.tsx"
+import DocsProvider from "./context/docs/DocsProvider.tsx"
+import { ThemeContextProvider } from "./context/theme/ThemeContextProvider.tsx"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<ThemeContextProvider>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
+			<DocsProvider>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</DocsProvider>
 		</ThemeContextProvider>
 	</StrictMode>,
 )
