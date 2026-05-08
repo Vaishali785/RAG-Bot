6931fe7b5e006c777482dc589b8f70dd6befb4f5
Vaishali <dev.vaishali785@gmail.com>
2026-05-08 21:02:07 +0530

    updated details for prod

--- src/constants/queries.ts
+++ src/constants/queries.ts
@@ -1,5 +1,4 @@
 export const API_END_POINT = import.meta.env.VITE_API_URL
-// export const API_END_POINT_PROD = "https://rag-bot-6401.onrender.com/"
 
 export const DOCS_API = `${API_END_POINT}/docs-list`
 export const UPDATE_DOC_API = `${API_END_POINT}/upload`
