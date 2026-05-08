c9661db9ef5107fd808a63218965c5b756997d0c
Vaishali <dev.vaishali785@gmail.com>
2026-05-08 15:56:11 +0530

    backend queries

---
+++ src/constants/queries.ts
@@ -0,0 +1,7 @@
+export const API_END_POINT = "http://localhost:8000"
+
+export const DOCS_API = `${API_END_POINT}/docs-list`
+export const UPDATE_DOC_API = `${API_END_POINT}/upload`
+export const REMOVE_DOC_API = `${API_END_POINT}/remove-doc`
+
+export const CHAT_API = `${API_END_POINT}/chat`
