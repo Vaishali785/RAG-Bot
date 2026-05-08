90c051f892015f9a2a01c2c7b2c6c22163b500ff
Vaishali <dev.vaishali785@gmail.com>
2026-05-08 15:33:58 +0530

    added interactivity to theme toggle btn

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,6 @@
 import { Moon, Sun } from "lucide-react"
 import { useContext } from "react"
-import { ThemeContext } from "../context/ThemeContext"
+import { ThemeContext } from "../context/theme/ThemeContext"
 
 export const ThemeToggle = () => {
 	const { theme, toggleTheme } = useContext(ThemeContext)
@@ -9,7 +9,7 @@ export const ThemeToggle = () => {
 		<button
 			onClick={toggleTheme}
 			aria-label="Toggle theme"
-			className="w-10 h-10 rounded-full glass flex items-center justify-center hover:bg-white/80 dark:hover:bg-white/10 transition-colors"
+			className="w-10 h-10 rounded-full glass flex items-center justify-center hover:bg-(image:--gradient-button)! hover:text-white cursor-pointer dark:hover:bg-white/10 transition-all"
 		>
 			{theme ? <Sun className="w-4 h-4" /> : <Moon className="w-4 h-4" />}
 		</button>
