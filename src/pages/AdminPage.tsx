f54c5b8ec41f219616c74574c2a58f1861205561
Vaishali <dev.vaishali785@gmail.com>
2026-05-08 15:17:53 +0530

    updated admin page

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,6 +1,7 @@
 import { Settings } from "lucide-react"
 import { useState } from "react"
 import AppearanceTab from "../components/admin/AppearanceTab"
+import DocumentList from "../components/admin/DocumentList"
 import KnowledgeTab from "../components/admin/KnowledgeTab"
 import Tabs from "../components/admin/Tabs"
 import type { TabSlug } from "../types/app-types"
@@ -9,7 +10,7 @@ const AdminPage = () => {
 	const [activeTab, setActiveTab] = useState<TabSlug>("knowledge")
 	return (
 		<main className="w-full z-20 py-10 h-full overflow-y-scroll">
-			<div className="mx-auto w-6xl">
+			<div className="mx-auto md:w-3xl lg:w-6xl max-sm:mx-4 max-md:mx-10 max-md:w-auto ">
 				<div className="mb-8 animate-fade-up ">
 					<div className="flex gap-4">
 						<div className="inline-flex items-center gap-2 px-3 py-1.5 rounded-full glass text-xs font-medium text-foreground/70 mb-4">
@@ -26,7 +27,13 @@ const AdminPage = () => {
 				</div>
 
 				<Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
-				<KnowledgeTab activeTab={activeTab} />
+
+				{activeTab === "knowledge" && (
+					<>
+						<KnowledgeTab />
+						<DocumentList />
+					</>
+				)}
 				<AppearanceTab activeTab={activeTab} />
 			</div>
 		</main>
